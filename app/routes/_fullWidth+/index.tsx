08c4bae13d181f7c564e218ea3a047aa1e280b5d
Petr Pacas <petr@pacas.cz>
2024-09-05 23:12:58 +0200

    Check for events to show in index groups

--- app/routes/_fullWidth+/index.tsx
+++ app/routes/_fullWidth+/index.tsx
@@ -83,104 +83,112 @@ export default function Landing() {
           </h2>
         </div>
       </div>
-      <div className="bg-transparent">
-        <div className="mx-auto grid w-full max-w-7xl gap-8 px-4 py-8 sm:px-8 sm:py-16">
-          <h2 className="text-2xl sm:text-3xl">
-            🔎 Discover <strong>soulful events</strong> taking place in{" "}
-            <strong>Czech Republic</strong>
-          </h2>
-          <div className="grid gap-4">
-            {events.map((event) => (
-              <EventListCard
-                eventsIndexFiltersCountry
-                key={event.id}
-                categories={event.categories}
-                country={event.country}
-                location={event.location}
-                dateStart={event.dateStart}
-                dateEnd={event.dateEnd}
-                id={event.id}
-                slug={event.slug}
-                timeStart={event.timeStart}
-                timeEnd={event.timeEnd}
-                title={event.title}
-              />
-            ))}
-          </div>
-          <div className="grid items-center gap-8 sm:max-xl:flex sm:max-xl:justify-between xl:grid-cols-3 xl:gap-16">
-            <p className="text-lg md:text-xl xl:col-span-2">
-              and many more&hellip;
-            </p>
-            <Link
-              to="/events?country=CZ"
-              className="flex items-center justify-center gap-3 rounded-lg border border-transparent bg-amber-600 px-4 py-2 text-lg text-white shadow-sm transition-shadow hover:shadow-md active:shadow sm:px-8 sm:py-4 sm:max-xl:col-start-3 sm:max-xl:justify-self-end lg:self-center"
-            >
-              Discover events in Czechia
-              <svg
-                className="h-6 w-6 max-[339px]:hidden"
-                width="16px"
-                height="16px"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-              >
-                <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
-              </svg>
-            </Link>
-          </div>
+      {(events.length > 0 || festivals.length > 0) && (
+        <div className="bg-transparent">
+          {events.length > 0 && (
+            <div className="mx-auto grid w-full max-w-7xl gap-8 px-4 py-8 sm:px-8 sm:py-16">
+              <h2 className="text-2xl sm:text-3xl">
+                🔎 Discover <strong>soulful events</strong> taking place in{" "}
+                <strong>Czech Republic</strong>
+              </h2>
+              <div className="grid gap-4">
+                {events.map((event) => (
+                  <EventListCard
+                    eventsIndexFiltersCountry
+                    key={event.id}
+                    categories={event.categories}
+                    country={event.country}
+                    location={event.location}
+                    dateStart={event.dateStart}
+                    dateEnd={event.dateEnd}
+                    id={event.id}
+                    slug={event.slug}
+                    timeStart={event.timeStart}
+                    timeEnd={event.timeEnd}
+                    title={event.title}
+                  />
+                ))}
+              </div>
+              <div className="grid items-center gap-8 sm:max-xl:flex sm:max-xl:justify-between xl:grid-cols-3 xl:gap-16">
+                <p className="text-lg md:text-xl xl:col-span-2">
+                  and many more&hellip;
+                </p>
+                <Link
+                  to="/events?country=CZ"
+                  className="flex items-center justify-center gap-3 rounded-lg border border-transparent bg-amber-600 px-4 py-2 text-lg text-white shadow-sm transition-shadow hover:shadow-md active:shadow sm:px-8 sm:py-4 sm:max-xl:col-start-3 sm:max-xl:justify-self-end lg:self-center"
+                >
+                  Discover events in Czechia
+                  <svg
+                    className="h-6 w-6 max-[339px]:hidden"
+                    width="16px"
+                    height="16px"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
+                    <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          )}
+          {events.length > 0 && festivals.length > 0 && (
+            <div className="border-t border-amber-600" />
+          )}
+          {festivals.length > 0 && (
+            <div className="mx-auto grid w-full max-w-7xl gap-8 px-4 py-8 sm:px-8 sm:py-16">
+              <h2 className="text-2xl sm:text-3xl">
+                ✨ Or, get enchanted at <strong>healing festivals</strong> all
+                around the <strong>world</strong>
+              </h2>
+              <div className="grid gap-4">
+                {festivals.map((event) => (
+                  <EventListCard
+                    key={event.id}
+                    categories={event.categories}
+                    country={event.country}
+                    location={event.location}
+                    dateStart={event.dateStart}
+                    dateEnd={event.dateEnd}
+                    id={event.id}
+                    slug={event.slug}
+                    timeStart={event.timeStart}
+                    timeEnd={event.timeEnd}
+                    title={event.title}
+                  />
+                ))}
+              </div>
+              <div className="grid items-center gap-8 sm:max-xl:flex sm:max-xl:justify-between xl:grid-cols-3 xl:gap-16">
+                <p className="text-lg md:text-xl xl:col-span-2">
+                  and more still&hellip;
+                </p>
+                <Link
+                  to="/events?category=festival"
+                  className="flex items-center justify-center gap-3 rounded-lg border border-amber-600 bg-transparent px-4 py-2 text-lg text-amber-600 shadow-sm transition-shadow hover:shadow-md active:shadow sm:px-8 sm:py-4 sm:max-xl:col-start-3 sm:max-xl:justify-self-end lg:self-center"
+                >
+                  Discover all the festivals
+                  <svg
+                    className="h-6 w-6 max-[339px]:hidden"
+                    width="16px"
+                    height="16px"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
-        <div className="border-t border-amber-600" />
-        <div className="mx-auto grid w-full max-w-7xl gap-8 px-4 py-8 sm:px-8 sm:py-16">
-          <h2 className="text-2xl sm:text-3xl">
-            ✨ Or, get enchanted at <strong>healing festivals</strong> all
-            around the <strong>world</strong>
-          </h2>
-          <div className="grid gap-4">
-            {festivals.map((event) => (
-              <EventListCard
-                key={event.id}
-                categories={event.categories}
-                country={event.country}
-                location={event.location}
-                dateStart={event.dateStart}
-                dateEnd={event.dateEnd}
-                id={event.id}
-                slug={event.slug}
-                timeStart={event.timeStart}
-                timeEnd={event.timeEnd}
-                title={event.title}
-              />
-            ))}
-          </div>
-          <div className="grid items-center gap-8 sm:max-xl:flex sm:max-xl:justify-between xl:grid-cols-3 xl:gap-16">
-            <p className="text-lg md:text-xl xl:col-span-2">
-              and more still&hellip;
-            </p>
-            <Link
-              to="/events?category=festival"
-              className="flex items-center justify-center gap-3 rounded-lg border border-amber-600 bg-transparent px-4 py-2 text-lg text-amber-600 shadow-sm transition-shadow hover:shadow-md active:shadow sm:px-8 sm:py-4 sm:max-xl:col-start-3 sm:max-xl:justify-self-end lg:self-center"
-            >
-              Discover all the festivals
-              <svg
-                className="h-6 w-6 max-[339px]:hidden"
-                width="16px"
-                height="16px"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
-                />
-              </svg>
-            </Link>
-          </div>
-        </div>
-      </div>
+      )}
       <div className="grid gap-8 pb-8 sm:gap-16 sm:pb-16">
         <div className="bg-amber-100">
           <div className="mx-auto grid w-full max-w-7xl gap-8 px-4 py-8 sm:gap-16 sm:px-8 sm:py-16 xl:grid-cols-2">
