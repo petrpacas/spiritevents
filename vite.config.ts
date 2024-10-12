ee33f538b47b12e0852c19776bafae0e48f2289b
Petr Pacas <petr@pacas.cz>
2024-10-12 14:28:05 +0200

    Bump packages + enable single-fetch

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import { flatRoutes } from "remix-flat-routes";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+declare module "@remix-run/server-runtime" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
 export default defineConfig({
   build: {
     sourcemap: true,
@@ -14,6 +20,7 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        v3_singleFetch: true,
       },
       ignoredRouteFiles: ["**/*"],
       routes: async (defineRoutes) => {
